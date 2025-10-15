043567093591aee11b393509dadbad08946f6619
Stefan Karczewski <s27459@pjwstk.edu.pl>
2025-10-15 22:03:28 +0200

    nix: Simplify flake

--- flake.nix
+++ flake.nix
@@ -8,29 +8,19 @@
     { systems, nixpkgs, ... }:
     let
       eachSystem =
-        f:
-        nixpkgs.lib.genAttrs (import systems) (
-          system:
-          f {
-            inherit system;
-            pkgs = import nixpkgs { inherit system; };
-          }
-        );
+        f: nixpkgs.lib.genAttrs (import systems) (system: f (import nixpkgs { inherit system; }));
     in
     {
-      devShells = eachSystem (
-        { system, pkgs }:
-        {
-          default = pkgs.mkShell {
-            packages = with pkgs; [
-              python313
-              uv
-            ];
-            shellHook = ''
+      devShells = eachSystem (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            python313
+            uv
+          ];
+          shellHook = ''
 
-            '';
-          };
-        }
-      );
+          '';
+        };
+      });
     };
 }
